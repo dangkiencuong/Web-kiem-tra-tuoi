35c70b4260eb6093b05db2e3fdc5d80639d87f34
dangkiencuong <dangkiencuong282011@gmail.com>
2026-09-01 01:16:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import streamlit as st
 
-st.title("Tra Cuu Truong Trung Tuyen")
+st.title("Tra Cứu Trường Trúng Tuyển,(đừng tin)")
 
 # Nhập tổng điểm thi
-diem = st.number_input("Nhap diem cua ban:", min_value=0.0, max_value=40.0, value=19.0)
+diem = st.number_input("Nhập điểm của bạn:", min_value=0.0, max_value=40.0, value=19.0)
 
-if st.button("Kiem tra ngay"):
+if st.button("Kiểm tra ngay"):
     st.write("---")
-    st.subheader("Ket qua xet tuyen:")
+    st.subheader("Kết quả xét tuyển:")
     
     # 1. Trường điểm cao nhất
     if diem >= 19.25:
-        st.success("Ban du diem do truong: Thanh Oai B (Moc 19.25)")
+        st.success("Bạn đủ điểm đỗ trường: Thanh Oai B (Mốc 19.25)")
         
     # 2. Trường điểm tiếp theo
     if diem >= 16.5:
-        st.success("Ban du diem do truong: Thanh Oai A (Moc 16.5)")
+        st.success("Bạn đủ điểm đỗ trường: Thanh Oai A (Mốc 16.5)")
         
     # 3. Trường điểm tiếp theo
     if diem >= 15.25:
-        st.success("Ban du diem do truong: Ly Tu Tan (Moc 15.25)")
+        st.success("Bạn đủ điểm đỗ trường: Lý Tự Tấn (Mốc 15.25)")
         
     # 4. Trường điểm sàn
     if diem >= 8.0:
-        st.success("Ban du diem do truong: Thanh Xuan (Moc 8.0)")
+        st.success("Bạn đủ điểm đỗ trường: Thanh Xuân (Mốc 8.0)")
         
     # 5. Dưới mốc sàn -> Báo rớt
     if diem < 8.0:
-        st.error("Diem cua ban chua du do truong nao trong danh sach.")
+        st.error("Điểm của bạn chưa đủ cho trường nào trong danh sách.")
         
