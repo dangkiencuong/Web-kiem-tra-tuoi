b813bfa36087af7a04b94d45b03fae7986cea8e4
dangkiencuong <dangkiencuong282011@gmail.com>
2026-09-02 20:13:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ if st.button("Kiểm tra ngay"):
             <div class="happy-emoji" style="left: 80%; animation-delay: 2,8s;">🥳</div>
             <div class="happy-emoji" style="left: 85%; animation-delay: 2,7s;">🥳</div>
             <div class="happy-emoji" style="left: 23%; animation-delay: 2,3s;">🥑</div>
-            <div style="left: 25%; animation-delay: 2,5s;"><img src="https://lens.usercontent.google.com/image?vsrid=CObkh7Hk5oTKtAEQARgBIhBHSE1ouOTRAjb_T_NGrQEVMoIBIgJzbygrQnQKLmxmZS1kdW1teTo0NjNhNTVjNS1lNjZiLTRiYjktYjFkMi00MWI5OGExZWM3MTASQgpAL2Jucy9zby9ib3JnL3NvL2Jucy9sZW5zLWZyb250ZW5kLWFwaS9wcm9kLmxlbnMtZnJvbnRlbmQtYXBpLzIyNFoECgJzbzjo3-bJ-c-WA1AAagppbWFnZS9qcGVn" style="width: 50px;"></div>
             """,
             unsafe_allow_html=True
         )
